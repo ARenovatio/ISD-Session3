bfaf4cc2235f3ac79b62ba4e1bfca62eecbafed0
Alessandro <43992008+ARenovatio@users.noreply.github.com>
2018-10-09 15:16:04 +0100

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,19 @@
+mynumber = int(input(" Please enter first number "))
+mynumber2 = int(input(" Please enter second number"))
+print ("the summary is", (mynumber + mynumber2))
+
+num=4
+print ("num is", num)
+num*=2
+print("num is", num)
+num1=num+2
+print("num is",num1) 
+num1+=3
+print("num is", num1)
+
+print(2/3)
+print(2//3)
+#the double backslash does not visualize decimals
+
+
+
